97c7d7d31dfddbd19fa4a75028fb4a7687c8d632
OCFormula <105538842+OCFormula@users.noreply.github.com>
2022-10-13 04:32:17 +0900

    Update and rename 우분투  /addfirefoxppa.sh to 우분투   /addfirefoxppa.sh

--- 우분투   /addfirefoxppa.sh
+++ 우분투   /addfirefoxppa.sh
@@ -3,3 +3,5 @@
 add-apt-repository -y ppa:mozillateam/firefox-next
 
 echo -e 'Package: *\nPin: release o=LP-PPA-mozillateam-firefox-next\nPin-Priority: 600' | sudo tee /etc/apt/preferences.d/mozillateam-firefox-next-pin-600
+
+apt install -y firefox
