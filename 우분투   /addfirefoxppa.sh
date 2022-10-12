e17e2a153a813531770229393117d3abb603f13b
OCFormula <105538842+OCFormula@users.noreply.github.com>
2022-10-13 04:37:47 +0900

    Update addfirefoxppa.sh

--- 우분투   /addfirefoxppa.sh
+++ 우분투   /addfirefoxppa.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+snap remove firefox
+
 add-apt-repository -y ppa:mozillateam/firefox-next
 
 echo -e 'Package: *\nPin: release o=LP-PPA-mozillateam-firefox-next\nPin-Priority: 600' | sudo tee /etc/apt/preferences.d/mozillateam-firefox-next-pin-600
