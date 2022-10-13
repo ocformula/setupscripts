848844651b26cc6ba4ea5db49810dac10735d805
OCFormula <105538842+OCFormula@users.noreply.github.com>
2022-10-13 19:00:59 +0900

    Update addfirefoxppa.sh

--- ubuntu/addfirefoxppa.sh
+++ ubuntu/addfirefoxppa.sh
@@ -2,7 +2,7 @@
 
 sudo snap remove firefox
 
-add-apt-repository -y ppa:mozillateam/firefox-next
+sudo add-apt-repository -y ppa:mozillateam/firefox-next
 
 echo -e 'Package: *\nPin: release o=LP-PPA-mozillateam-firefox-next\nPin-Priority: 600' | sudo tee /etc/apt/preferences.d/mozillateam-firefox-next-pin-600
 
