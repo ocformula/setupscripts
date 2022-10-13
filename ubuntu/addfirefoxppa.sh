497beea7e7829eb3781e9557918e41017a962814
OCFormula <105538842+OCFormula@users.noreply.github.com>
2022-10-13 19:02:50 +0900

    Update addfirefoxppa.sh

--- ubuntu/addfirefoxppa.sh
+++ ubuntu/addfirefoxppa.sh
@@ -6,4 +6,4 @@ sudo add-apt-repository -y ppa:mozillateam/firefox-next
 
 echo -e 'Package: *\nPin: release o=LP-PPA-mozillateam-firefox-next\nPin-Priority: 600' | sudo tee /etc/apt/preferences.d/mozillateam-firefox-next-pin-600
 
-apt install -y firefox
+sudo apt install -y firefox
