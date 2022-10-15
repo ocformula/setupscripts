1cfe2859f793dd786cfa412ccd752f01cac44f30
OCFormula <105538842+OCFormula@users.noreply.github.com>
2022-10-15 14:33:01 +0900

    Update installlutris.sh

--- debian/installlutris.sh
+++ debian/installlutris.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-https://lutris.net/downloads
+# https://lutris.net/downloads
 
 wget -O- https://download.opensuse.org/repositories/home:/strycore/Debian_11/Release.key | gpg --dearmor | sudo tee /usr/share/keyrings/lutris-keyring.gpg > /dev/null
 echo "deb [signed-by=/usr/share/keyrings/lutris-keyring.gpg] http://download.opensuse.org/repositories/home:/strycore/Debian_11/ ./" | sudo tee /etc/apt/sources.list.d/lutris.list
